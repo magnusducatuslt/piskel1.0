4c91dbb1ec0456ad295bc25b6d9910cc5483f61c
Artemis4e <lisovski112017@mail.ru>
2019-06-02 20:09:11 +0300

    feat: full-screen of animation

--- src/components/animationBoard/animation.js
+++ src/components/animationBoard/animation.js
@@ -1,16 +1,30 @@
 import React, { Fragment } from 'react';
 import './animation.css';
 
+const full = () => {
+  var elem = document.getElementById('ww');
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
 const Animation = () => {
   return (
     <Fragment>
-      <div className='animation-window'>
+      <div id='ww' className='animation-window'>
         window
       </div>
       <div className='animation-config'>
         <button>2 FPS</button>
         <button>4 FPS</button>
         <button>8 FPS</button>
+        <button onClick={full}>full screen</button>
       </div>
     </Fragment>
   );
