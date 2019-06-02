b6157f03b792d907ddc98620851db5331202e51c
Your Name <you@example.com>
2019-06-02 22:10:54 +0300

    feat: start animation

--- src/components/animationBoard/animation.js
+++ src/components/animationBoard/animation.js
@@ -5,22 +5,25 @@ const full = () => {
   var elem = document.getElementById('ww');
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
     elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari & Opera */
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
     elem.msRequestFullscreen();
   }
-}
+};
 
 const Animation = () => {
   return (
     <Fragment>
-      <div id='ww' className='animation-window'>
+      <div id="ww" className="animation-window">
         window
       </div>
-      <div className='animation-config'>
+      <div className="animation-config">
         <button>2 FPS</button>
         <button>4 FPS</button>
         <button>8 FPS</button>
@@ -30,4 +33,37 @@ const Animation = () => {
   );
 };
 
+function animate() {
+  var requestId;
+
+  function loop(time) {
+    requestId = undefined;
+
+    doStuff(time);
+    start();
+  }
+
+  function start() {
+    if (!requestId) {
+      requestId = window.requestAnimationFrame(loop);
+    }
+  }
+
+  function stop() {
+    if (requestId) {
+      window.cancelAnimationFrame(requestId);
+      requestId = undefined;
+    }
+  }
+
+  function doStuff(time) {}
+
+  document.querySelector('#start').addEventListener('click', function() {
+    start();
+  });
+
+  document.querySelector('#stop').addEventListener('click', function() {
+    stop();
+  });
+}
 export default Animation;
