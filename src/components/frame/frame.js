c3f40bdfc1f198312830b1920baac148c7a32869
Artemis4e <lisovski112017@mail.ru>
2019-06-02 18:03:54 +0300

    feat:frame-images and styles

--- src/components/frame/frame.js
+++ src/components/frame/frame.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './frame.css';
+import del from '../../img/delete.png'
+import cop from '../../img/copy.png'
+import con from '../../img/drag.png'
+
 const Frame = ({ onDubl, onDel, index }) => {
   return (
     <div className="frame">
-      <span>{index}</span>
-      <div onClick={onDel}>delete</div>
-      <div onClick={onDubl}>clone</div>
+      <div className="top">
+        <div>{index}</div>
+        <div onClick={onDel}>
+          <img src={del} alt="delete"></img>
+        </div>
+      </div>
+      <div className="bottom">
+        <div>
+          <img src={con} alt="config"></img>
+        </div>
+        <div onClick={onDubl}>
+          <img src={cop} alt="clone"></img>
+        </div>
+      </div>
     </div>
   );
 };
